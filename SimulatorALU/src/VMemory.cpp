9305bc734305add1a58b5cea442fa88ac50f627e
Jonathan Dunlap <jadit2@gmail.com>
2013-09-03 07:58:56 -0700

    new proj files

---
+++ SimulatorALU/src/VMemory.cpp
@@ -0,0 +1,18 @@
+/*
+ * VMemory.cpp
+ *
+ *  Created on: Sep 2, 2013
+ *      Author: jonathan
+ */
+
+#include "VMemory.h"
+
+VMemory::VMemory() {
+	// TODO Auto-generated constructor stub
+
+}
+
+VMemory::~VMemory() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ SimulatorALU/src/VMemory_test.cpp
@@ -0,0 +1,9 @@
+/*
+ * VMemory_test.cpp
+ *
+ *  Created on: Sep 2, 2013
+ *      Author: jonathan
+ */
+
+#include "VMemory.h"
+
