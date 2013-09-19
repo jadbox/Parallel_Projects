ca3f5862fbf0be3d609770071fed8cbf1184ae22
Jonathan Dunlap <jdunlpa@outlook.com>
2013-09-19 00:17:34 -0700

    sleep tested

--- ParallelTeamApp/src/ParallelTeam.cpp
+++ ParallelTeamApp/src/ParallelTeam.cpp
@@ -10,12 +10,15 @@
 #include <iostream>
 #include <string>
 #include <stdlib.h>   // Declaration for exit() and rand()
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <time.h>
 
 using namespace std;
 
 ParallelTeam::ParallelTeam():pids() {
 	// TODO Auto-generated constructor stub
-	cout << "ParallelTeam started";
+	cout << "ParallelTeam started" << endl;
 }
 
 ParallelTeam::~ParallelTeam() {
@@ -28,23 +31,28 @@ void ParallelTeam::createProcessTeam(int n) {
 	for(int i = 0; i < n; i++) {
 		t = fork();
 		if(t < 0) {
-			cout << "error spawning process";
-			exit(1);
+			cout << "error spawning process" << endl;
+			_exit(1);
 		}
 		else if(t==0) {
+			srand(i * time(NULL));
 			int x = 1 + rand() % 7;
-			sleep(x*1000);
-			cout << "Process ended";
-			exit(0);
+			sleep(x);
+			cout << "Process ended " << x << endl;
+			_exit(0);
 		}
 		else if(t > 0) {
+
 			pids.push_back(t);
 		}
 	}
-
 	if(t > 0) {
-		cout << "Parent: process ended";
-		exit(0);
+		int status;
+		for(int i = 0; i < n; i++) {
+			//pid_t wid =
+					waitpid(-1, &status, 0);
+			cout << "Parent: process ended" << endl;
+		}
 	}
 }
 
