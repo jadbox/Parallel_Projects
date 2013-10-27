cf393f1de920925f2c2b37b1e06a2a6f5a1b6da2
Jonathan Dunlap <jdunlpa@outlook.com>
2013-10-27 13:28:46 -0700

    mpi example

---
+++ Count3sMPI/src/Count3sMPI.cpp
@@ -0,0 +1,39 @@
+
+////////////////////////////////////////////////////////
+
+#include "Count3sMPI.h"
+using namespace std;
+
+int main(int argc, char** argv) {
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &numProcs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    lengthProc = size/numProcs;
+
+    if (rank == RootProcess) {
+        for (int p=0; p < numProcs-1; p++) {
+            MPI_Send(a, lengthProc, MPI_INT, p+1, tag, MPI_COMM_WORLD);
+        }
+    } else {
+        MPI_Recv(a, lengthProc, MPI_INT, RootProcess, tag, MPI_COMM_WORLD, &status);
+    }
+
+    //generates local count
+    for (int i=0; i < lengthProc; i++) {
+        if (a[i] == 3)
+            localCount++;
+        cout << localCount << endl;
+    }
+
+    //reduce count
+    MPI_Reduce(&localCount, &globalCount, 1, MPI_INT, MPI_SUM, RootProcess, MPI_COMM_WORLD);
+
+    //prints total count
+    if (rank == RootProcess)
+        cout << "Number of 3's: " << globalCount << endl;
+
+    //terminates program
+    MPI_Finalize();
+    return 0;
+}
---
+++ Count3sMPI/src/Count3sMPI.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include "mpi.h"
+
+#pragma once
+
+    //size: 12 //3s: 4
+    int a[] = {23, 1, 3, 5, 2, 3, 2, 1, 2, 1, 3, 3};
+    int size = 12;
+
+    int numProcs = 3;
+
+    int rank;
+    int lengthProc;
+    int localCount;
+    int globalCount;
+    int RootProcess = 0;
+    MPI_Status status;
+    int tag = 1;
